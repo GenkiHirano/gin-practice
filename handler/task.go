09c3974e21deb2f4ceff110b7fc5b0e6f74ddf71
GenkiHirano <g.hirano@gemcook.com>
2022-03-01 15:43:17 +0900

    Gin (実践編)

--- handler/task.go
+++ handler/task.go
@@ -1,17 +1,13 @@
 package handler
 
 import (
-	"net/http"
 	"time"
-
-	"github.com/GenkiHirano/gin-practice/repository"
-	"github.com/gin-gonic/gin"
 )
 
 type requestUser struct {
 	ID      int    `json:"id"`
 	Name    string `json:"name"`
-	Comment string `json:"comment" binding:"required,max=10"`
+	Comment string `json:"comment"`
 }
 
 type responseUser struct {
@@ -21,25 +17,8 @@ type responseUser struct {
 	CreatedAt time.Time `json:"createdAt"`
 }
 
-func CreateUser() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		var req requestUser
-		if err := c.Bind(&req); err != nil {
-			c.JSON(http.StatusBadRequest, err.Error())
-		}
+// CreateUserを実装してください。
+// 引数、戻り値、関数の中身はお好きに変更可能です。
+func CreateUser() {
 
-		createdUser, err := repository.Create(req.ID, req.Name, req.Comment)
-		if err != nil {
-			c.JSON(http.StatusBadRequest, err.Error())
-		}
-
-		res := responseUser{
-			ID:        createdUser.ID,
-			Name:      createdUser.Name,
-			Comment:   createdUser.Comment,
-			CreatedAt: createdUser.CreatedAt,
-		}
-
-		c.JSON(http.StatusCreated, res)
-	}
 }
--- main.go
+++ main.go
@@ -8,7 +8,10 @@ import (
 func main() {
 	r := gin.Default()
 
+	// Ginでユーザー登録のルーティングを書いてください。
+	// パス/userがリクエストされた時に、ユーザー登録を処理してください。
 	r.POST("/user", handler.CreateUser())
 
+	// Ginでサーバー起動の処理を書いてください。
 	r.Run()
 }
